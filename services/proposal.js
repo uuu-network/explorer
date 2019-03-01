2599098c64e685e0593f996acc1f19bf5792df72
eijgong <397076254@qq.com>
2019-03-01 16:44:52 +0800

    UNetwork on-chain governance updates.

--- services/proposal.js
+++ services/proposal.js
@@ -1,5 +1,10 @@
 const CONST = require('./const.js')
 const db = require('../diskdb.js')
+const crypto = require('crypto')
+
+var sd = require('silly-datetime');
+var time = sd.format(new Date())
+
 
 module.exports = ($scope) => {
   web3 = $scope.web3
@@ -11,16 +16,39 @@ module.exports = ($scope) => {
       function  propose() {
         var rsContract = new web3.eth.Contract(proposalContractAbi, contractAddress)
             // console.log(rsContract)
-        var startTime = parseInt(query.proposalStartTime)
-        var duration  = parseInt(query.proposalDuration)
-//        var cost = parseInt(query.proposalCost)
-          
-        var proposalHash = web3.utils.fromDecimal(query.proposalHash)
+
+        var startTimeDate = new Date(query.proposalStartTime)
+        var startTime = startTimeDate.getTime()/1000 + 60 * 60 * 7
+
+        var endTimeDate = new Date(query.proposalEndTime)
+        var endTime = endTimeDate.getTime()/1000 + 60 * 60 * 7
+        var duration = endTime - startTime
+
+        if(startTime < Date.now()/1000 +10 ) {
+          return cb({err: 6, msg: "The start time of this proposal has passed." })
+        }
+        if(duration > 60 * 60 *24 * 30){
+          return cb({err: 7, msg: "The duration of this proposal is too long."})
+        }
+
+        var md5 = crypto.createHash('md5')
+        var _proposalHash = '0x' + md5.update(query.proposalDetail, 'utf8').digest('hex')
+        var proposalobjs = db.proposals.find({proposalHash: _proposalHash})
+        console.log(_proposalHash)
+//        console.log(proposalobjs)
+//        var proposalobj = new Array();
+        if (proposalobjs.length) {
+          return cb({err:8, msg: "This proposal has already been proposed."})
+        }
+        //        console.log(proposalHash)
+        // console.log(_proposalHash)
+        //        var proposalHash = web3.utils.fromDecimal(query.proposalHash)
 //        var hex = stringtohex
-        rsContract.methods.propose(proposalHash, startTime, duration)
+//        console.log(_proposalHash)
+        rsContract.methods.propose(_proposalHash, startTime, duration)
         .send({
             from: accobj.address,
-            gas: 200000,
+            gas: 20000000,
             gasPrice: query.usefreegas==='1' ? '0' : web3.eth.currentGasPrice,
             chainId: 5816,
             value: web3.utils.toWei(query.proposalCost, 'ether').toString()
@@ -28,6 +56,19 @@ module.exports = ($scope) => {
             if(err){
             return cb({err: 5, msg: err})
             }
+            // TODO： 后续用event监听上链结果，以判断是否加入数据库
+            db.proposals.save({
+              userName:           query.userName,
+              userProfile:        query.userProfile,
+              proposalTitle:      query.proposalTitle,
+              proposalSummary:    query.proposalSummary,
+              proposalDetail:     query.proposalDetail,
+              proposalStartTime:  query.proposalStartTime,
+              proposalEndTime:    query.proposalEndTime,
+              proposalDuration:   duration,
+              proposalHash:       _proposalHash,
+            })
+            //console.log()
             cb(null, { status: 'ok', trshash: value })
         })
       }  
@@ -47,8 +88,8 @@ module.exports = ($scope) => {
         var rsContract = new web3.eth.Contract(proposalContractAbi, contractAddress)
         // console.log(rsContract)
         var voteOption = parseInt(query.voteOption)
-//        var proposalHash = web3.utils.fromDecimal(query.proposalHash)
-        rsContract.methods.vote(query.proposalHash, voteOption)
+        console.log(query.proposalHash)
+        rsContract.methods.vote( query.proposalHash, voteOption)
         .send({
             from: accobj.address,
             gas: 200000,
@@ -103,6 +144,8 @@ module.exports = ($scope) => {
       function  withdraw() {
         var rsContract = new web3.eth.Contract(proposalContractAbi, contractAddress)
 //        var _value = parseInt(query.value)
+        console.log(query.value)
+        console.log(web3.utils.toWei(query.value, 'ether'))
         rsContract.methods.withdraw(web3.utils.toWei(query.value, 'ether'))
         .send({
             from: accobj.address,
@@ -130,6 +173,8 @@ module.exports = ($scope) => {
       function voteResults() {
         var rsContract = new web3.eth.Contract(proposalContractAbi, contractAddress)
 //        var proposalHash = web3.utils.fromDecimal(query.proposalHash)
+        console.log(123213123)
+        console.log()
         rsContract.methods.results(query.proposalHash, query.voteOption)
         .call({
             chainId: 5816,
@@ -137,7 +182,7 @@ module.exports = ($scope) => {
             if(err){
             return cb({err: 5, msg: err})
             }
-            cb(null, { status: 'ok', trshash: value })
+            cb(null, { status: 'ok', voteResult: value })
         })
       }  
 
@@ -149,10 +194,496 @@ module.exports = ($scope) => {
         cb({err: 5, msg: 'Call VoteResults error:' + e})
       }
     },
+
+    depositsOf: function(query, cb, req){
+      function depositsOf(userobj) {
+
+        console.log(userobj.address)
+        var rsContract = new web3.eth.Contract(proposalContractAbi, contractAddress)
+//        var proposalHash = web3.utils.fromDecimal(query.proposalHash)
+        rsContract.methods.depositsOf(userobj.address)
+        .call({
+            chainId: 5816,
+        }, (err, value) => {
+            console.log(value, userobj.address)
+            if(err){
+            return cb({err: 5, msg: err})
+            }
+            console.log(value)
+            cb(null, { status: 'ok', votes: web3.utils.fromWei(value, 'ether') })
+        })
+      }  
+      var accobj = CONST.checkLoginForApi(web3, req, cb)
+      if( !accobj ) return
+      try{
+        depositsOf(accobj);
+      }catch(e){
+        cb({err: 5, msg: 'Call depositsOf error:' + e})
+      }
+    },
+
+    getActiveProposal: function(query, cb, req){
+      function getActiveProposal() {
+        var proposalobjs = db.proposals.find()
+        var proposalobj = new Array();
+        for(var _i in proposalobjs){
+          var proposal = proposalobjs[_i]
+          console.log(proposal)
+          console.log(Date.now())
+          var startTime = new Date(proposal.proposalStartTime)
+          startTime = startTime.getTime()/1000 + 60 * 60 * 7
+          var endTime = new Date(proposal.proposalEndTime)
+          endTime = endTime.getTime()/1000 + 60 * 60 * 7
+
+          if(startTime <= Date.now()/1000 
+          && endTime >= Date.now()/1000) {
+            proposalobj.push(proposal)
+          }
+        }
+        console.log(startTime)
+        console.log(proposalobjs)
+        console.log(proposalobj)
+        cb(null, {datas: proposalobj})
+      }
+      try{
+        getActiveProposal()
+      }catch(e){
+        cb({err: 5, msg: 'Call activeProposals error:' + e})
+      }
+    },
+
+    getLeaderBoard: function(query, cb, req){
+      function getLeaderBoard() {
+        var proposalobjs = db.proposals.find()
+        var proposalobj = new Array();
+//        console.log(proposalobjs)
+        for(var _i in proposalobjs){
+          var proposal = proposalobjs[_i]
+
+          var endTime = new Date(proposal.proposalEndTime)
+          endTime = endTime.getTime()/1000 + 60 * 60 * 7
+//          console.log(proposal.proposalEndTime, endTime)
+          if(endTime <= Date.now()/1000) {
+            // console.log(proposal)
+            // console.log(_i, endTime, Date.now()/1000)
+            proposalobj.push(proposal)
+          }
+        }
+//        console.log(proposalobjs)
+//        console.log(proposalobj)
+        cb(null, {datas: proposalobj})
+      }
+      try{
+        getLeaderBoard()
+      }catch(e){
+        cb({err: 5, msg: 'Call LeaderBoard error:' + e})
+      }
+    }
+
   }
 }
 
 // This var should be changed to a new address of your contract.
-const contractAddress = '';
+const contractAddress = '0xdd7fc4849580c96647660c92dab2e3dba25339e4';
 
-const proposalContractAbi = [{"constant":false,"inputs":[{"name":"_payment","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_cost","type":"uint256"}],"name":"changeProposalCost","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes"},{"name":"_vote","type":"uint8"}],"name":"results","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"voteCost","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_payee","type":"address"}],"name":"mortageInfo","outputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_duration","type":"uint256"}],"name":"changeMortage","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_cost","type":"uint256"}],"name":"changeVoteCost","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"payee","type":"address"}],"name":"depositsOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"proposalCost","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes"},{"name":"_startTime","type":"uint256"},{"name":"_duration","type":"uint256"}],"name":"propose","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes"},{"name":"_vote","type":"uint8"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_proposalcostI","type":"uint256"},{"name":"_voteCostI","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_proposer","type":"address"},{"indexed":false,"name":"_hash","type":"bytes"},{"indexed":false,"name":"_startTime","type":"uint256"}],"name":"Proposed","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_voter","type":"address"},{"indexed":false,"name":"_hash","type":"bytes"},{"indexed":false,"name":"_vote","type":"uint8"}],"name":"Voted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"payee","type":"address"},{"indexed":false,"name":"weiAmount","type":"uint256"}],"name":"Deposited","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"payee","type":"address"},{"indexed":false,"name":"weiAmount","type":"uint256"}],"name":"Withdrawn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}]
+const proposalContractAbi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_duration",
+				"type": "uint256"
+			}
+		],
+		"name": "changeMortage",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_cost",
+				"type": "uint256"
+			}
+		],
+		"name": "changeProposalCost",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_cost",
+				"type": "uint256"
+			}
+		],
+		"name": "changeVoteCost",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_hash",
+				"type": "bytes"
+			},
+			{
+				"name": "_startTime",
+				"type": "uint256"
+			},
+			{
+				"name": "_duration",
+				"type": "uint256"
+			}
+		],
+		"name": "propose",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_hash",
+				"type": "bytes"
+			},
+			{
+				"name": "_vote",
+				"type": "uint8"
+			}
+		],
+		"name": "vote",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_payment",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_proposalcostI",
+				"type": "uint256"
+			},
+			{
+				"name": "_voteCostI",
+				"type": "uint256"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_proposer",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "_hash",
+				"type": "bytes"
+			},
+			{
+				"indexed": false,
+				"name": "_startTime",
+				"type": "uint256"
+			}
+		],
+		"name": "Proposed",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_voter",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "_hash",
+				"type": "bytes"
+			},
+			{
+				"indexed": false,
+				"name": "_vote",
+				"type": "uint8"
+			}
+		],
+		"name": "Voted",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "payee",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "weiAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "Deposited",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "payee",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "weiAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "Withdrawn",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "payee",
+				"type": "address"
+			}
+		],
+		"name": "depositsOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "isOwner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_payee",
+				"type": "address"
+			}
+		],
+		"name": "mortageInfo",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "proposalCost",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_hash",
+				"type": "bytes"
+			}
+		],
+		"name": "proposalInfo",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_hash",
+				"type": "bytes"
+			},
+			{
+				"name": "_vote",
+				"type": "uint8"
+			}
+		],
+		"name": "results",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "voteCost",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
--- services/tube.js
+++ services/tube.js
@@ -16,7 +16,7 @@ module.exports = ($scope) => {
               // console.log(rsContract)
               rsContract.methods.getCoin(address)
               .send({
-                  from: ownerAddress,
+                  from: accobj.address,
                   gas: 100000,
                   gasPrice: query.usefreegas==='1' ? '0' : web3.eth.currentGasPrice,
                   chainId: 5816,
@@ -40,9 +40,307 @@ module.exports = ($scope) => {
     }
   }
 }
-// Thie var should be change later.
-const ownerAddress = '0xb11CC363119182bFA5c8641f76B1414A93fA5988' ;
 // This var should be changed to a new address of your contract.
-const contractAddress = '0xeAE5A6643E7E47a0BE6bD9eccbA92fEAF4e1bb75';
+const contractAddress = '0xa0eec7a3578fd8ee17e83d00131444fcf71693d9';
 
-const tubeContractAbi = [{"constant":false,"inputs":[{"name":"_payee","type":"address"}],"name":"getCoin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"coinNumber","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_payee","type":"address"}],"name":"userInfo","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_number","type":"uint256"}],"name":"changeCoinNumber","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"readyCoin","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"updateCoin","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"blockTimestamp","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_time","type":"uint256"}],"name":"changeUpdateTime","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"funds","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"digit","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"readyTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"updateTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_coin","type":"uint256"}],"name":"changeUpdateCoin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_time","type":"uint256"},{"name":"_coin","type":"uint256"},{"name":"_number","type":"uint256"}],"payable":true,"stateMutability":"payable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"time","type":"uint256"},{"indexed":false,"name":"coins","type":"uint256"}],"name":"Update","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"coins","type":"uint256"}],"name":"GetCoin","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}]
+const tubeContractAbi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_number",
+				"type": "uint256"
+			}
+		],
+		"name": "changeCoinNumber",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_coin",
+				"type": "uint256"
+			}
+		],
+		"name": "changeUpdateCoin",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_time",
+				"type": "uint256"
+			}
+		],
+		"name": "changeUpdateTime",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_payee",
+				"type": "address"
+			}
+		],
+		"name": "getCoin",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_time",
+				"type": "uint256"
+			},
+			{
+				"name": "_coin",
+				"type": "uint256"
+			},
+			{
+				"name": "_number",
+				"type": "uint256"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "time",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "coins",
+				"type": "uint256"
+			}
+		],
+		"name": "Update",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "coins",
+				"type": "uint256"
+			}
+		],
+		"name": "GetCoin",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "coinNumber",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "digit",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "funds",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "isOwner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "readyCoin",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "readyTime",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "updateCoin",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "updateTime",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "userUsed",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
